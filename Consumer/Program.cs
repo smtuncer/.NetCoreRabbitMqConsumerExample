536d50c37c59c61de07ef5c7cc4d0681942e3c50
smtun <smtuncer123@gmail.com>
2023-03-13 10:56:20 +0300

    .NetCoreRabbitMqConsumerExample
    .Net Core 6 RabbitMq Consumer Example

---
+++ Consumer/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Threading.Channels;
+
+// connectionFactory --> Connection ---> Channel 
+var connectionFactory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    //If you want to remote connection 
+    //Uri = new Uri(""),
+    //UserName = "",
+    //Password = "",
+};
+var connection = connectionFactory.CreateConnection();
+var channel = connection.CreateModel();
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (sender, args) =>
+{
+    var byteMessage = args.Body.ToArray();
+    var message = Encoding.UTF8.GetString(byteMessage);
+    Console.WriteLine("Message: " + message);
+};
+
+channel.BasicConsume(queue: "Test", autoAck: true, consumer: consumer);
+
+Console.WriteLine("..");
+Console.ReadKey();
